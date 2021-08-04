1281390cfb2ad9b332317d764c8f100d50140479
Benoit Crozemarie <benoit.crozemarie@gmail.com>
2021-08-04 23:18:01 +0200

    feat(temtem): add ServiceApi, fix(temtem):icon temtem

---
+++ src/app/temtem.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {Temtem} from "./model/temtem";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TemtemService {
+
+  url ="https://temtem-api.mael.tech/api/temtems/";
+  constructor(private http:HttpClient) { }
+
+  findAll(){
+    return this.http.get<Temtem[]>(this.url);
+  }
+}
